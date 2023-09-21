a642caa960200f6dbf11913a8bada71a688fe758
Kirill Misyuro <kmisyuro@gmail.com>
2023-09-21 19:03:05 +0300

    Add age column

---
+++ ClinicApp/HelperClasses/AgeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ClinicApp.HelperClasses
+{
+    internal class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateOfBirth)
+            {
+                int age = DateTime.Now.Year - dateOfBirth.Year;
+                if (DateTime.Now < dateOfBirth.AddYears(age))
+                {
+                    age--;
+                }
+                return age;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
